baba1a116b211a1807b9a0ff16a692c8eafa7322
Nick Nisi <nick@nisi.org>
2012-10-22 16:44:11 -0500

    Initial work on the script

--- tasks/it.js
+++ tasks/it.js
@@ -22,6 +22,43 @@ module.exports = function (grunt) {
         grunt.log.write(grunt.helper('it'));
     });
 
+    grunt.registerMultiTask('it', 'execute it unit tests', function () {
+        var it = require('it'),
+            comb = require('comb'),
+            config = grunt.config.get('it'),
+            files = config.files,
+            path = require('path');
+
+        var filepaths = grunt.file.expandFiles(this.file.src);
+        grunt.file.clearRequireCache(filepaths);
+
+        var paths = filepaths.map(path.resolve),
+            options = this.data.options || {},
+            reporter = options.reporter || 'dotmatrix';
+
+        var done = this.async();
+        return comb.serial(comb.array.compact(paths.map(function (f) {
+            var suite = require(f);
+            suite.reporter = reporter;
+            return suite.run ? suite.run.bind(suite) : null;
+        }))).then(function (results) {
+            var its = {};
+            var errors = 0;
+            results.forEach(function (summary) {
+                comb(summary).forEach(function (testData) {
+                    comb(testData.summaries).forEach(function (summaryData) {
+                        if (summaryData.status === 'failed') {
+                            errors++;
+                        }
+                    });
+                });
+                comb.merge(its, summary);
+            });
+            it.printSummary(its);
+            done(0 === errors);
+        });
+    });
+
     // ==========================================================================
     // HELPERS
     // ==========================================================================
