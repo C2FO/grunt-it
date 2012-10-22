e43044f190397804f586530e301d07da98de82f1
Nick Nisi <nick@nisi.org>
2012-10-22 16:40:42 -0500

    formatting updates

--- tasks/it.js
+++ tasks/it.js
@@ -1,3 +1,4 @@
+/*jshint strict:false*/
 /*
  * grunt-it
  * https://github.com/nicknisi/grunt-it
@@ -5,26 +6,28 @@
  * Copyright (c) 2012 Nick Nisi
  * Licensed under the MIT license.
  */
+module.exports = function (grunt) {
 
-module.exports = function(grunt) {
+    // Please see the grunt documentation for more information regarding task and
+    // helper creation: https://github.com/gruntjs/grunt/blob/master/docs/toc.md
 
-  // Please see the grunt documentation for more information regarding task and
-  // helper creation: https://github.com/gruntjs/grunt/blob/master/docs/toc.md
+    // ==========================================================================
+    // TASKS
+    // ==========================================================================
 
-  // ==========================================================================
-  // TASKS
-  // ==========================================================================
+    grunt.registerTask('it', 'Execute it tests', function () {
+        var config = grunt.config.get('it'),
+            dirs = config.dirs || '/',
+            reporter = config.reporter || 'dotmatrix';
+        grunt.log.write(grunt.helper('it'));
+    });
 
-  grunt.registerTask('it', 'Your task description goes here.', function() {
-    grunt.log.write(grunt.helper('it'));
-  });
+    // ==========================================================================
+    // HELPERS
+    // ==========================================================================
 
-  // ==========================================================================
-  // HELPERS
-  // ==========================================================================
-
-  grunt.registerHelper('it', function() {
-    return 'it!!!';
-  });
+    grunt.registerHelper('it', function () {
+        return 'it!!!';
+    });
 
 };
--- test/it_test.js
+++ test/it_test.js
@@ -1,34 +1,35 @@
+/*jshint strict:false*/
 var grunt = require('grunt');
 
 /*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
+   ======== A Handy Little Nodeunit Reference ========
+https://github.com/caolan/nodeunit
 
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
+Test methods:
+test.expect(numAssertions)
+test.done()
+Test assertions:
+test.ok(value, [message])
+test.equal(actual, expected, [message])
+test.notEqual(actual, expected, [message])
+test.deepEqual(actual, expected, [message])
+test.notDeepEqual(actual, expected, [message])
+test.strictEqual(actual, expected, [message])
+test.notStrictEqual(actual, expected, [message])
+test.throws(block, [error], [message])
+test.doesNotThrow(block, [error], [message])
+test.ifError(value)
 */
 
 exports['it'] = {
-  setUp: function(done) {
-    // setup here
-    done();
-  },
-  'helper': function(test) {
-    test.expect(1);
-    // tests here
-    test.equal(grunt.helper('it'), 'it!!!', 'should return the correct value.');
-    test.done();
-  }
+    setUp: function (done) {
+        // setup here
+        done();
+    },
+    'helper': function (test) {
+        test.expect(1);
+        // tests here
+        test.equal(grunt.helper('it'), 'it!!!', 'should return the correct value.');
+        test.done();
+    }
 };
